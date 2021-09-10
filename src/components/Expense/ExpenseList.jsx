95f2ddafd36bc14afdc0b5f853a128914c274bf9
Singh, Vikramjit <viksingh@rbbn.com>
2021-09-11 01:22:35 +0530

    Added the expense filter component

---
+++ src/components/Expense/ExpenseList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import ExpenseItem from "./ExpenseItem";
+
+import './ExpenseList.css';
+
+const ExpenseList = (props) => {
+    const { expenseItems } = props;
+
+    if (expenseItems.length === 0) {
+        return (
+            <h2 className='expenses-list__fallback'>Found no expenses.</h2>
+        );
+    }
+    return (
+        <ul className='expenses-list'>
+            { expenseItems.map(({id, title, amount, date}) => {
+                return (
+                    <ExpenseItem
+                        key={id}
+                        title={title}
+                        amount={amount}
+                        date={date}
+                    />
+                );
+            })}
+        </ul>
+    );
+};
+
+export default ExpenseList;
--- src/components/Expense/Expenses.jsx
+++ src/components/Expense/Expenses.jsx
@@ -1,22 +1,32 @@
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from 'react';
+
+import ExpenseList from "./ExpenseList";
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [selectedFilter, setSelectedFilter] = useState('2021');
+
+    const onSelectedFilterHandler = (e) => {
+        setSelectedFilter(e.target.value);
+    };
+
     const { expenses } = props;
+    const expenseItems = expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === selectedFilter;
+    });
+
     return (
         <Card className="expenses">
-            {expenses.map(({id, title, amount, date}) => {
-                return (
-                    <ExpenseItem
-                        key={id}
-                        title={title}
-                        amount={amount}
-                        date={date}
-                    />
-                );
-            })}
+            <ExpenseFilter
+                selectedFilter={selectedFilter}
+                onSelectedFilterHandler={onSelectedFilterHandler}
+            />
+            <ExpenseList
+                expenseItems={expenseItems}
+            />
         </Card>
     );
 };
